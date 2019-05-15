2280652079c93998a427e21ed49cf5271d43cb5d
Markus Döring <m.doering@mac.com>
2019-05-15 11:49:59 +0200

    Adding genus names

--- generate-coldp.py
+++ generate-coldp.py
@@ -37,63 +37,75 @@ def replBracketAuthors(m):
         parsedAuthors.append(m.group(1))
         return ""
 
+def processRow(row, rank=None):
+    global parsedAuthors
+    id      = row[idCol]
+    if "code" in id:
+        return
+    refID = None
+    ref = "|".join([row[sourceCol], row[yearCol], row[pagesCol]])
+    if ref:
+        if ref in refs:
+            refID = refs[ref]
+        else:
+            refID = "r"+id
+            refs[ref] = refID
+            reference.writerow([refID, row[yearCol], row[sourceCol], row[pagesCol]])
+    parsedAuthors = []
+    auth    = brackMatcher.sub("", unescape(row[authCol]))
+    sciname = brackAuthMatcher.sub(replBracketAuthors, unescape(row[nameCol]))
+    if sciname.isupper():
+        # genera are all upper
+        sciname = sciname.capitalize()
+    remarks = unescape(row[remarksCol])
+    if parsedAuthors:
+        auth = " ".join(parsedAuthors + [auth])
+        #print(sciname + "  ||  " + auth)
+    name.writerow([id, rank, sciname, auth, refID, row[yearCol], row[assessmentCol], remarks, row[linkCol]])
+    if row[basionymCol]:
+        m = idMatcher.search(row[basionymCol])
+        if m:
+            nameRel.writerow([id, m.group(1), "basionym"])
+        else:
+            print("Basionym without ID: " + row[basionymCol])
+    if row[misspellingCol]:
+        m = asMatcher.search(row[misspellingCol])
+        if m:
+            # usually a full name, but sometime with comments and weird stuff we simply ignore
+            if badMatcher.search(m.group(1)):
+                print("BAD misspelling: " + m.group(1))
+                misspelling = None
+            else:
+                misspelling = unescape(m.group(1))
+                print("Unparsed misspelling: " + misspelling)
+        else:
+            # epithet only
+            misspelling = sciname.rpartition(' ')[0]  + ' ' + unescape(row[misspellingCol])
 
-with open('extract_output.csv', 'r') as inFile:
-    species = csv.reader(inFile, delimiter='\t')
-    with open('name.tsv', 'w', newline='') as nameFile:
-        name = csv.writer(nameFile, delimiter='\t')
-        name.writerow(["ID","scientificName","authorship", "publishedInID", "publishedInYear", "status", "remarks", "link"])
-        with open('name-rel.tsv', 'w', newline='') as nameRelFile:
-            nameRel = csv.writer(nameRelFile, delimiter='\t')
-            nameRel.writerow(["nameID","relatedNameID","type"])
-            with open('reference.tsv', 'w', newline='') as referenceFile:
-                reference = csv.writer(referenceFile, delimiter='\t')
-                reference.writerow(["ID","year","source","details"])
-                refs  = {}
+        if misspelling:
+            #print("Misspelling: " + misspelling)
+            idAlt = id + "-alt"
+            name.writerow([idAlt, None, misspelling, auth, refID, row[yearCol], row[assessmentCol], remarks, row[linkCol]])
+            nameRel.writerow([id, idAlt, "spelling correction"])
+
+
+
+
+with open('name.tsv', 'w', newline='') as nameFile:
+    name = csv.writer(nameFile, delimiter='\t')
+    name.writerow(["ID","rank", "scientificName","authorship", "publishedInID", "publishedInYear", "status", "remarks", "link"])
+    with open('name-rel.tsv', 'w', newline='') as nameRelFile:
+        nameRel = csv.writer(nameRelFile, delimiter='\t')
+        nameRel.writerow(["nameID","relatedNameID","type"])
+        with open('reference.tsv', 'w', newline='') as referenceFile:
+            reference = csv.writer(referenceFile, delimiter='\t')
+            reference.writerow(["ID","year","source","details"])
+            refs  = {}
+            with open('extract_output.csv', 'r') as inFile:
+                species = csv.reader(inFile, delimiter='\t')
                 for row in species:
-                    id      = row[idCol]
-                    if "code" in id:
-                        continue
-                    refID = None
-                    ref = "|".join([row[sourceCol], row[yearCol], row[pagesCol]])
-                    if ref:
-                        if ref in refs:
-                            refID = refs[ref]
-                        else:
-                            refID = "r"+id
-                            refs[ref] = refID
-                            reference.writerow([refID, row[yearCol], row[sourceCol], row[pagesCol]])
-                    parsedAuthors = []
-                    auth    = brackMatcher.sub("", unescape(row[authCol]))
-                    sciname = brackAuthMatcher.sub(replBracketAuthors, unescape(row[nameCol]))
-                    remarks = unescape(row[remarksCol])
-                    if parsedAuthors:
-                        auth = " ".join(parsedAuthors + [auth])
-                        #print(sciname + "  ||  " + auth)
-                    name.writerow([id, sciname, auth, refID, row[yearCol], row[assessmentCol], remarks, row[linkCol]])
-                    if row[basionymCol]:
-                        m = idMatcher.search(row[basionymCol])
-                        if m:
-                            nameRel.writerow([id, m.group(1), "basionym"])
-                        else:
-                            print("Basionym without ID: " + row[basionymCol])
-                    if row[misspellingCol]:
-                        m = asMatcher.search(row[misspellingCol])
-                        if m:
-                            # usually a full name, but sometime with comments and weird stuff we simply ignore
-                            if badMatcher.search(m.group(1)):
-                                print("BAD misspelling: " + m.group(1))
-                                misspelling = None
-                            else:
-                                misspelling = unescape(m.group(1))
-                                print("Unparsed misspelling: " + misspelling)
-                        else:
-                            # epithet only
-                            misspelling = sciname.rpartition(' ')[0]  + ' ' + unescape(row[misspellingCol])
-
-                        if misspelling:
-                            #print("Misspelling: " + misspelling)
-                            idAlt = id + "-alt"
-                            name.writerow([idAlt, misspelling, auth, refID, row[yearCol], row[assessmentCol], remarks, row[linkCol]])
-                            nameRel.writerow([id, idAlt, "spelling correction"])
-
+                     processRow(row)
+            with open('extract_genus.csv', 'r') as inFile:
+                genera = csv.reader(inFile, delimiter='\t')
+                for row in genera:
+                     processRow(row, rank='genus')                    
